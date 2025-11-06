e56d7032edaca5ab0250c2a39b67e0375f2c72af
vitusberinggg <virrebering@gmail.com>
2025-11-06 11:26:39 +0100

    added some function comments

--- sender/sender.py
+++ sender/sender.py
@@ -11,7 +11,20 @@ delimiter_duration = 0.5
 
 # --- Helper functions ---
 
-def show_color(color, duration = binary_duration):
+def update_screen_color(color, duration = binary_duration):
+
+    """
+    Updates the screen with the given color.
+
+    Arguments:
+        "color"
+        "duration"
+
+    Returns:
+        None
+
+    """
+
     canvas.config(background = color)
     root.update()
     time.sleep(duration)
@@ -20,22 +33,40 @@ def show_color(color, duration = binary_duration):
 
 def send_message(message):
 
-    for character in message:
-        bits = format(ord(character), "08b")
+    """
+    Sends the message by calling "update_screen_color".
 
-        for bit in bits:
-            show_color("white" if bit == "1" else "black")
+    Arguments:
+        "message"
 
-        show_color("red", delimiter_duration)
+    Returns:
+        None
+    
+    """
+
+    for character in message: # For each character in the message:
+
+        bits = format(ord(character), "08b") # Convert the character to it's corresponding ASCII chain of 1's and 0's
+
+        for bit in bits: # For each bit:
+
+            if bit == "1":
+                update_screen_color("white")
+
+            else:
+                update_screen_color("black")
+
+        update_screen_color("red", delimiter_duration)
 
 # --- GUI setup ---
 
 root = tk.Tk()
 root.attributes("-fullscreen", True)
+
 canvas = tk.Canvas(root, highlightthickness = 0)
 canvas.pack(fill = "both", expand = True)
 
 # --- Main execution ---
 
-time.sleep(2)
+time.sleep(1)
 send_message("HELLO")
