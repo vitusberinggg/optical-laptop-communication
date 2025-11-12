41abf9f4291bde37ab4944c7d5bf815d44c6af4e
vitusberinggg <virrebering@gmail.com>
2025-11-12 11:31:43 +0100

    created reciever v3

---
+++ reciever/reciever_v3.py
@@ -0,0 +1,11 @@
+
+# --- Imports ---
+
+import cv2
+
+# --- Definitions ---
+
+sender_output_width = 1920
+sender_output_height = 1200
+
+reference_image_seed = 42
--- sender/sender_v3.py
+++ sender/sender_v3.py
@@ -4,7 +4,6 @@
 import cv2
 import numpy as np
 import time
-import math
 
 # ---- Definitions ----
 
