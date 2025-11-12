06d0970a4b7e1ae2e7b7e8005c455880719cb10f
vitusberinggg <virrebering@gmail.com>
2025-11-12 13:47:21 +0100

    split sender up into smaller modules in utilities

--- sender/sender_v3.py
+++ sender/sender_v3.py
@@ -5,120 +5,22 @@ import cv2
 import numpy as np
 import time
 
+from utilities.generate_reference_image import generate_reference_image
+from utilities.message_to_frame_bit_arrays import message_to_frame_bit_arrays
+from utilities.render_frame import render_frame
+
+from utilities.global_definitions import (
+    sender_output_height, sender_output_width,
+    reference_image_duration,
+    frame_duration)
+
 # ---- Definitions ----
 
 message = "HELLO"
 
-output_width = 1920
-output_height = 1200
-
-reference_image_seed = 42
-reference_image_duration = 2.0
-
 fps = 30
 
-columns = 8
-rows = 8
-
-bit_time = 0.5
-
-bit_cell_width = output_width // columns
-bit_cell_height = output_height // rows
-
 # --- Helper functions ---
-
-def generate_reference_image():
-
-    """
-    Generates the reference image by putting the image seed into a bit generator.
-
-    Arguments:
-        None
-
-    Returns:
-        "reference_image": The reference image.
-
-    """
-
-    bit_generator = np.random.RandomState(reference_image_seed)
-    reference_image = bit_generator.randint(0, 256, (output_height, output_width), dtype = np.uint8)
-
-    return reference_image
-
-def message_to_frame_bit_arrays(message):
-
-    """
-    Turns a message into a list of frames represented as 2D NumPy arrays of bits.
-
-    Arguments:
-        "message" (str): The message to be converted.
-
-    Returns:
-        "frame_bit_arrays": A list of 2D NumPy arrays representing the frames.
-    
-    """
-
-    binary_list = []
-
-    for character in message: # For each character in the message:
-        ascii_value = ord(character) # Convert it to ASCII
-        binary_string = format(ascii_value, "08b") # Format the value as an 8-bit binary string
-        binary_list.append(binary_string) # Add the string to the binary list
-    
-    bits = "".join(binary_list) # Merge all strings in the binary list into a single string
-
-    frame_capacity = rows * columns
-    frame_bit_arrays = []
-
-    for start_index in range(0, len(bits), frame_capacity): # For each starting index in the range 0 - len(bits), stepping with the frame capacity:
-
-        chunk = bits[start_index:start_index + frame_capacity] # Slice a chunk the size of the frame capacity from the string of bits
-
-        if len(chunk) < frame_capacity: # If the length of the chunk is smaller than the frame capacity
-            chunk = chunk.ljust(frame_capacity, "0") # Pad with 0's until the chunk is the same size as the frame capacity
-
-        frame_array = np.array(list(chunk), dtype = np.uint8).reshape((rows, columns)) # Convert the chunk to a list, convert the list into an NumPy array (more efficient) and then reshape the one-dimensional array into a 2D array
-
-        frame_bit_arrays.append(frame_array) # Add the frame array into the list of frames
-
-    return frame_bit_arrays
-
-def render_frame(bitgrid):
-
-    """
-    Renders a frame from a NumPy array.
-
-    Arguments:
-        "bitgrid" (np.ndarray): A 2D NumPy array of bits.
-
-    Returns:
-        "image" (np.ndarray): A NumPy array representing the image frame pixels.
-
-    """
-
-    image = np.zeros((output_height, output_width, 3), dtype = np.uint8)
-
-    for row in range(rows): # For each row:
-
-        for column in range(columns): # For each column:
-
-            bit = int(bitgrid[row, column]) # Get the bit at the current position
-
-            if bit == 1: # If the bit is 1:
-                color = (255, 255, 255) # Set the color to white
-            
-            else: # Else (if the bit is 0):
-                color = (0, 0, 0) # Set the color to black
-
-            start_x_coordinate = column * bit_cell_width
-            end_x_coordinate = start_x_coordinate + bit_cell_width
-
-            start_y_coordinate = row * bit_cell_height
-            end_y_coordinate = start_y_coordinate + bit_cell_height
-
-            cv2.rectangle(image, (start_x_coordinate, start_y_coordinate), (end_x_coordinate - 1, end_y_coordinate - 1), color, thickness = -1) # Draw the rectangle on the image
-
-    return image
 
 def create_color_frame(color):
 
@@ -133,7 +35,7 @@ def create_color_frame(color):
 
     """
 
-    return np.full((output_height, output_width, 3), color, dtype = np.uint8)
+    return np.full((sender_output_height, sender_output_width, 3), color, dtype = np.uint8)
 
 # --- Main function ---
 
@@ -161,7 +63,6 @@ def send_message(message = "HELLO"):
         rendered_frame = render_frame(frame_bit_array) # Render the frame
         data_frames.append(rendered_frame) # Add the rendered frame to the list of data frames
 
-    sync_frame = create_color_frame((0, 255, 0))
     end_frame  = create_color_frame((0, 0, 255))
     black_frame = create_color_frame((0, 0, 0))
 
@@ -183,18 +84,8 @@ def send_message(message = "HELLO"):
 
         while True:
 
-            sync_start = time.time()
-
-            while time.time() - sync_start < 0.3:
-
-                cv2.imshow(win, sync_frame)
-
-                if cv2.waitKey(int(1000 / fps)) & 0xFF == ord('q'):
-                    return
-
             for frame in data_frames:
 
-                frame_duration = bit_time
                 frame_start = time.time()
 
                 while time.time() - frame_start < frame_duration:
---
+++ utilities/global_definitions.py
@@ -0,0 +1,16 @@
+
+# --- General definitions ---
+
+reference_image_seed = 42
+reference_image_duration = 2
+
+sender_output_width = 1920
+sender_output_height = 1200
+
+number_of_columns = 8
+number_of_rows = 8
+
+bit_cell_width = sender_output_width // number_of_columns
+bit_cell_height = sender_output_height // number_of_rows
+
+frame_duration = 0.3
