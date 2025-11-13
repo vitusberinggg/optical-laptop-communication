7fbe3291a4a40c9183dd14f21eb63c58460bac48
klarapalsson <klara.palsson@ericsson.com>
2025-11-13 15:34:14 +0100

    Update decode_bitgrid.py

--- utilities/decode_bitgrid.py
+++ utilities/decode_bitgrid.py
@@ -3,7 +3,7 @@
 
 import numpy as np
 
-from global_definitions import number_of_rows, number_of_columns, bit_cell_height, bit_cell_width, cell_brightness_threshold
+from utilities.global_definitions import number_of_rows, number_of_columns, bit_cell_height, bit_cell_width, cell_brightness_threshold
 
 # --- Main function ---
 
