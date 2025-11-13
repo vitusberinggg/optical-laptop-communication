70d48a0e699339debc60117829c7535e39d5dc7b
klarapalsson <klara.palsson@ericsson.com>
2025-11-13 14:56:13 +0100

    Update detect_end_frame.py

--- utilities/detect_end_frame.py
+++ utilities/detect_end_frame.py
@@ -3,7 +3,7 @@
 
 import numpy as np
 
-from global_definitions import end_frame_color, end_frame_detection_tolerance
+from utilities.global_definitions import end_frame_color, end_frame_detection_tolerance
 
 # --- Main function ---
 
