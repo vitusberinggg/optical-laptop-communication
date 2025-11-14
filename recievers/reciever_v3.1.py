b8b0a42146b14763113008be7fc03e85490de4ad
vitusberinggg <virrebering@gmail.com>
2025-11-14 14:22:29 +0100

    added more to recieve_message

--- recievers/reciever_v3.1.py
+++ recievers/reciever_v3.1.py
@@ -18,6 +18,7 @@ from utilities.global_definitions import (
 # --- Definitions ---
 
 camera_index = 0 # Index of the camera to be used
+camera = cv2.VideoCapture(camera_index) # Initialize video capture from the specified camera index
 
 # --- Main function ---
 
@@ -34,18 +35,14 @@ def receive_message():
     
     """
 
-    camera = cv2.VideoCapture(camera_index) # Initialize video capture from the specified camera index
-
     print("[INFO] Waiting for ArUco marker frame...")
-
-    detected_homography_matrix = None
-    warped_frame_shape = (sender_output_height, sender_output_width)
 
     while True:
 
         read_was_successful, frame = camera.read() # Read the frame and the boolean indicating if the read was successful or not
         
         if not read_was_successful: # If the read wasn't successful:
+            print("[WARNING] Can't read the camera.")
             continue # Skip this iteration
 
 #       Green screen detection
@@ -108,7 +105,7 @@ def receive_message():
 
         frame = cv2.bitwise_and(frame, frame, mask = aruco_marker_mask) # Applies the ArUco marker mask to the captured frame
 
-        reference_images_match = detect_ecc_reference_image(frame, ecc_reference_image) # Check if the frame matches good enough with the reference image
+        reference_images_match = detect_ecc_reference_image(frame, ecc_reference_image) # Checks if the frame matches good enough with the reference image
 
         if reference_images_match:
 
@@ -128,75 +125,7 @@ def receive_message():
             print("[WARNING] Sender screen doesn't match reference image.")
             continue
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    reference_image_height, reference_image_width = reference_image.shape # Get the dimensions of the reference image
-    reference_image_float32 = reference_image.astype(np.float32) # Converts the reference image to float32 format
-
-    ecc_warp_mode = cv2.MOTION_AFFINE # Sets the ECC warp mode to affine
-    ecc_warp_matrix = np.eye(2, 3, dtype = np.float32) # Initializes the ECC warp matrix as an identity matrix
-    ecc_criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 5000, 1e-7) # Sets the ECC criteria for the alignment process
-
-    reference_image_detected = False
-    reference_start_time = None
-    ecc_computed = False
-
-    bitstring = ""
-    frame_counter = 0
-    end_frame_detected = False
-
-    print("[INFO] Waiting for the reference frame...")
-
-    while True:
-
-        snapshots = []
-
-        for _ in range(samples_per_frame): # For each sample:
-
-            for _ in range(2):
-                videoCapture.grab() # Grab two frames to ensure the latest frame is captured (mini buffer flush)
-
-            return_value, frame = videoCapture.read() # Capture a frame from the video feed
-
-            if not return_value: # If the frame capture was unsuccessful:
-                continue # Skip to the next iteration
-
-            frame_grayscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # 
-
-            snapshots.append(frame_grayscale.astype(np.float32))
-
-            time.sleep(sample_space)
-
-        if not snapshots: # If there aren't any snapshots:
-            continue # Skip to the next iteration
-
-        average_grayscaled_frame = np.mean(snapshots, axis=0).astype(np.float32)
-
-        if not reference_image_detected: # If no reference image has been detected:
-            if (average_grayscaled_frame.shape[1], average_grayscaled_frame.shape[0]) != (reference_image_width, reference_image_height): # If the captured frame isn't the same size as the reference image:
-
-    videoCapture.release()
-    cv2.destroyAllWindows()
-
-    if end_frame_detected: # If the end frame is detected:
-        message = bits_to_message(bitstring) # Decode the bitstrig
-        print(message) # Print the decoded message
-
-    else:
-        print("\n[INFO] No end frame detected, transmission may be incomplete.")
+    print("[INFO] Ready to recieve data.")
 
 # --- Execution ---
 
