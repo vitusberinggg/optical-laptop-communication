9d60c9bbbd32f122078096189d4dd21773b38a67
klarapalsson <klara.palsson@ericsson.com>
2025-11-17 10:33:18 +0100

    Update sender_v2.py

--- senders/sender_v2.py
+++ senders/sender_v2.py
@@ -101,6 +101,9 @@ def message_to_frames(message, bit_time = 0.35, fps = 15):
     f = create_frame(center_color = (0, 255, 0))
     frames += [f] * samples_per_bit * 3 
 
+    f = create_frame(center_color = (255, 0, 0))
+    frames += [f] * samples_per_bit  
+
     for character in message:
         bits = format(ord(character), "08b")
 
@@ -117,10 +120,10 @@ def message_to_frames(message, bit_time = 0.35, fps = 15):
 
             f = create_frame(center_color = (255, 0, 0))
             frames += [f] * samples_per_bit
-        
+        """
         f = create_frame(center_color = (0, 0, 255))
         frames += [f] * samples_per_bit
-
+        """
     return frames
 
 def show_message(message):
