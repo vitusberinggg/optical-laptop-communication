109230637c18f52e8d5198045c189d0456a3f116
klarapalsson <klara.palsson@ericsson.com>
2025-11-20 11:00:54 +0100

    Update global_definitions.py

--- utilities/global_definitions.py
+++ utilities/global_definitions.py
@@ -37,7 +37,7 @@ bit_cell_height = sender_output_height // number_of_rows # Height of each bit ce
 
 frame_duration = 0.3  # Duration for each frame in seconds
 
-preamble_colors = [black, white, black, white]
+preamble_colors = [black, white, black, white, black, white]
 
 # --- Reciever input definitions ---
 
