0e99bb00c8d6f3edc9335ac518a1fecd8e75507b
loadofspaghetti <max.jaderstrom@ericsson.com>
2025-11-21 14:54:25 +0100

    m

--- utilities/detection_functions.py
+++ utilities/detection_functions.py
@@ -6,10 +6,8 @@ import numpy as np
 
 from utilities.global_definitions import (
     sender_output_height, sender_output_width,
-    reference_match_threshold,
-    start_frame_color, start_frame_detection_tolerance,
-    end_frame_color, end_frame_detection_tolerance,
-    aruco_marker_dictionary
+    reference_match_threshold, start_frame_detection_tolerance,
+    end_frame_detection_tolerance, aruco_marker_dictionary
 )
 
 # --- Functions ---
@@ -86,41 +84,3 @@ def detect_ecc_reference_image(potential_sync_frame, ecc_reference_image):
     max_correlation = cv2.minMaxLoc(correlation)[1] # Get the maximum correlation value
 
     return max_correlation > reference_match_threshold # Return True if the correlation is larger than the threshold, False if not
-
-def detect_start_frame(frame):
-
-    """
-    Detects if the average frame color is close to the start frame color.
-
-    Arguments:
-        "frame" (np.ndarray): A NumPy array representing the frame pixels.
-
-    Returns:
-        bool: True if the average frame color is close to the start frame color, False otherwise.
-    
-    """
-
-    average_frame_color = frame.mean(axis = (0, 1)) # Calculates the average color of the frame
-
-    distance = np.linalg.norm(average_frame_color - np.array(start_frame_color)) # Calculates the Euclidean distance between the average frame color and the start frame color
-
-    return distance < start_frame_detection_tolerance # Returns True if the distance is less than the tolerance, False otherwise
-
-def detect_end_frame(frame):
-
-    """
-    Detects if the average frame color is close to the end frame color.
-
-    Arguments:
-        "frame" (np.ndarray): A NumPy array representing the frame pixels.
-
-    Returns:
-        bool: True if the average frame color is close to the end frame color, False otherwise.
-    
-    """
-
-    average_frame_color = frame.mean(axis = (0, 1)) # Calculates the average color of the frame
-
-    distance = np.linalg.norm(average_frame_color - np.array(end_frame_color)) # Calculates the Euclidean distance between the average frame color and the end frame color
-
-    return distance < end_frame_detection_tolerance # Returns True if the distance is less than the tolerance, False otherwise
