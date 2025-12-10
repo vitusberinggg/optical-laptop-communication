0cdbd3dfe0dcbba8729b70c2febf1c002b4e25c8
Klara Pålsson <klara.palsson@ericsson.com>
2025-12-10 14:49:23 +0100

    rows

--- text/utilities/global_definitions.py
+++ text/utilities/global_definitions.py
@@ -104,7 +104,7 @@ sender_output_width = 1920 # Width of the sender output in pixels
 sender_output_height = 1200 # Height of the sender output in pixels
 
 number_of_columns = 16 # Number of columns in the frame
-number_of_rows = 16 # Number of rows in the frame
+number_of_rows = 12 # Number of rows in the frame
 
 bit_cell_width = sender_output_width // number_of_columns # Width of each bit cell in pixels
 bit_cell_height = sender_output_height // number_of_rows # Height of each bit cell in pixels
